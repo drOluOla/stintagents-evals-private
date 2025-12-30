c2a010b1b5bfecb7c296066aa4c83b02f679dcd5
Oluseyi <oluseyiolarewaju@outlook.com>
2025-12-30 15:59:51 +0000

    Removed deleted imports

--- stintagents/__init__.py
+++ stintagents/__init__.py
@@ -12,16 +12,8 @@ from .utils import (
     convert_audio_bytes,
     generate_speech_async,
     transcribe_audio_async,
-    transcribe_audio_batch_async,
     get_agent_response_with_speech,
     process_voice_input,
-    process_audio_batch,
-    process_audio_batch_async,
-    # Multi-GPU
-    enable_multi_gpu_transcription,
-    get_multi_gpu_pool,
-    MultiGPUTranscriptionPool,
-    GPUWorkerConfig,
 )
 
 from .ui import create_agent_avatar, create_gradio_interface
@@ -34,16 +26,8 @@ __all__ = [
     "convert_audio_bytes",
     "generate_speech_async",
     "transcribe_audio_async",
-    "transcribe_audio_batch_async",
     "get_agent_response_with_speech",
     "process_voice_input",
-    "process_audio_batch",
-    "process_audio_batch_async",
-    # Multi-GPU
-    "enable_multi_gpu_transcription",
-    "get_multi_gpu_pool",
-    "MultiGPUTranscriptionPool",
-    "GPUWorkerConfig",
     # UI
     "create_agent_avatar",
     "create_gradio_interface",
